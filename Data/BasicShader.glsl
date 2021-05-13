57d19bedd90221279b7e2592857a238e2d13a80c
Luka Aleksic <xxx@xxx.xxx>
2021-05-14 01:35:35 +0200

    Add shader file to git

---
+++ Data/BasicShader.glsl
@@ -0,0 +1,32 @@
+uniform mat4 ModelViewProjection;
+uniform sampler2D DiffuseTexture;
+
+#if defined(VERTEX_SHADER)
+    layout (location=0) in vec3 Position;
+    layout (location=1) in vec3 Color;
+    layout (location=2) in vec2 TexCoords;
+    out VertexData {
+        vec3 Color;
+        vec2 TexCoords;
+    } vertexData;
+
+    void main() {
+        gl_Position.xyz = Position;
+        gl_Position.w = 1.0f;
+        gl_Position = ModelViewProjection * gl_Position;
+        vertexData.Color = Color;
+        vertexData.TexCoords = TexCoords;
+    }
+#elif defined(FRAGMENT_SHADER)
+    in VertexData {
+        vec3 Color;
+        vec2 TexCoords;
+    } vertexData;
+
+    out vec4 color;
+
+    void main() {
+        color = texture(DiffuseTexture, vertexData.TexCoords);
+        color *= vec4(vertexData.Color, 1);
+    }
+#endif
