fe172e41417205427584fbdf2bea7cc5ff1bcc75
Luka Aleksic <xxx@xxx.xxx>
2021-05-14 20:10:47 +0200

    Make master header

--- main.cpp
+++ main.cpp
@@ -1,29 +1,5 @@
-#include <stdio.h>
-#include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/rotate_vector.hpp>
-#include <glfw/glfw3.h>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-#include <assimp/IOStream.hpp>
-#include <assimp/IOSystem.hpp>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#include <array>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <memory>
-#include <filesystem>
-using namespace glm;
-using namespace std;
-namespace fs = filesystem;
+#include "main.hpp"
 
 class Camera {
     // Pitch/Yaw are in degrees
---
+++ main.hpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glfw/glfw3.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <assimp/IOStream.hpp>
+#include <assimp/IOSystem.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include "stb_image.h"
+#include <array>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <memory>
+#include <filesystem>
+using namespace glm;
+using namespace std;
+namespace fs = filesystem;
